9c80e6806608c534ea5c7417a337956f8d803953
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2020-12-24 23:20:28 +0300

    Create recognition.js
    Template functions which is used to implement js side of dev.html

---
+++ app/recognition.js
@@ -0,0 +1,19 @@
+function lis1() {
+  alert("YOU CLICKED LISTEN1");
+  document.getElementById("output-content").innerHTML = "listen1";
+}
+
+function lis2() {
+//  alert("YOU CLICKED LISTEN2");
+  document.getElementById("output-content").innerHTML = "listen2";
+}
+
+function dev1() {
+//  alert("YOU CLICKED DEV1");
+  document.getElementById("output-content").innerHTML = "dev1";
+}
+
+function dev2() {
+//  alert("YOU CLICKED DEV2");
+  document.getElementById("output-content").innerHTML = "dev2";
+}
